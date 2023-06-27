8275c05293c086bd07a4b62e153087f011bb2835
Tanith Flory <tanithflory7770@gmail.com>
2023-06-27 16:43:26 +0530

    rebuild

--- client/src/Components/Registration/RegistrationForm.styles.ts
+++ client/src/Components/Registration/RegistrationForm.styles.ts
@@ -43,7 +43,7 @@ const SRegistrationForm = styled.form`
     background: #2f6ce5;
     width: 100%;
     border-radius: 20px;
-    max-width: 220px;
+    max-width: 175px;
     height: 40px;
     margin: 19px 0;
     border: none;
@@ -57,6 +57,9 @@ const SRegistrationForm = styled.form`
     align-items: center;
     width: 100%;
     max-width: 420px;
+    & > div {
+      flex-basis: 25%;
+    }
   }
   h5 {
     margin: 0 0 1rem;
