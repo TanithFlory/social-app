0531233f805846daf6dd3c2eb32225f3b1c2adfa
Tanith Flory <tanithflory7770@gmail.com>
2023-06-27 15:28:17 +0530

    build

--- server/index.ts
+++ server/index.ts
@@ -1,7 +1,8 @@
-import express, { Express, Request, Response } from "express";
+import express, { Express } from "express";
 import cors from "cors";
 import authRoutes from "./routes/authRoutes";
 import postRoutes from "./routes/postRoutes";
+import path from "path";
 const port: number = 3001;
 const app: Express = express();
 
@@ -10,6 +11,12 @@ app.use(cors());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/post", postRoutes);
+
+app.use(express.static(path.join(__dirname, "/client")));
+app.get("*", (_req, res) => {
+  const filePath = path.join(__dirname, "/client/index.html");
+  res.sendFile(filePath);
+});
 
 app.listen(port, () => {
   console.log(`Listening to ${port}`);
